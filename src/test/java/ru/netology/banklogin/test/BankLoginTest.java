119a5c653fe3e9479223692afb9f8befdd33104e
Anest <efremenko.ira.rzn@gmail.com>
2024-11-02 23:09:15 +0300

    Test1

---
+++ src/test/java/ru/netology/banklogin/test/BankLoginTest.java
@@ -0,0 +1,49 @@
+package ru.netology.banklogin.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.banklogin.data.DataHelper;
+import ru.netology.banklogin.data.SQLHelper;
+import ru.netology.banklogin.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.banklogin.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.banklogin.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    LoginPage loginPage;
+
+   @AfterEach
+   void tearDown() {
+       cleanAuthCodes();
+   }
+
+    @AfterAll
+   static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("https://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login and password from sut test data")
+    void shouldSuccessfulLogin() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.login(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+
+    }
+
+    @Test
+    @DisplayName("should get error notification if user is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.login(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверноуказан логин или пароль");
+    }
+
+}
